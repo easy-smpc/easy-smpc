e9b85f46620d9ce6981fd781e286e9fd4cea1937
Tobias Kussel <kussel@cbs.tu-darmstadt.de>
2020-05-05 12:14:02 +0200

    Refactor Model test

--- java/src/test/java/de/tu_darmstadt/cbs/emailsmpc/AppModelTest.java
+++ java/src/test/java/de/tu_darmstadt/cbs/emailsmpc/AppModelTest.java
@@ -14,11 +14,10 @@ public class AppModelTest {
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void AddingBins() {
+    static private AppModel getInitializedModel(int numParticipants, int numBins) {
         AppModel testmodel = new AppModel();
-        Participant[] part = new Participant[3];
-        Bin[] bins = new Bin[4];
+        Participant[] part = new Participant[numParticipants];
+        Bin[] bins = new Bin[numBins];
         for (int i = 0; i < part.length; i++) {
             part[i] = new Participant("Participant " + i, "part" + i + "@test.com");
         }
@@ -27,44 +26,43 @@ public class AppModelTest {
             bins[i].initialize(part.length);
             bins[i].shareValue(BigInteger.valueOf(3 * i + 7));
         }
-        testmodel.advanceState(AppState.STARTING);
-        testmodel.initializeStudy("Teststudy", part, bins);
+        testmodel.toStarting();
+        testmodel.toInitialSending("Teststudy", part, bins);
+        return testmodel;
+    }
+
+    static private AppModel getInitializedModel(int numParticipants, int numBins, BigInteger[] array) {
+        AppModel testmodel = new AppModel();
+        Participant[] part = new Participant[numParticipants];
+        Bin[] bins = new Bin[numBins];
+        for (int i = 0; i < part.length; i++) {
+            part[i] = new Participant("Participant " + i, "part" + i + "@test.com");
+        }
+        for (int i = 0; i < bins.length; i++) {
+            bins[i] = new Bin("Bin " + i);
+            bins[i].initialize(part.length);
+            bins[i].shareValue(array[i]);
+        }
+        testmodel.toStarting();
+        testmodel.toInitialSending("Teststudy", part, bins);
+        return testmodel;
+    }
+
+    @Test
+    public void AddingBins() {
+        AppModel testmodel = AppModelTest.getInitializedModel(3, 4);
         assertTrue(testmodel.bins.length == 4);
     }
 
     @Test
     public void AddingParticipants() {
-        AppModel testmodel = new AppModel();
-        Participant[] part = new Participant[3];
-        Bin[] bins = new Bin[4];
-        for (int i = 0; i < part.length; i++) {
-            part[i] = new Participant("Participant " + i, "part" + i + "@test.com");
-        }
-        for (int i = 0; i < bins.length; i++) {
-            bins[i] = new Bin("Bin " + i);
-            bins[i].initialize(part.length);
-            bins[i].shareValue(BigInteger.valueOf(3 * i + 7));
-        }
-        testmodel.advanceState(AppState.STARTING);
-        testmodel.initializeStudy("Teststudy", part, bins);
+        AppModel testmodel = AppModelTest.getInitializedModel(3, 4);
         assertTrue(testmodel.participants.length == 3);
     }
 
     @Test
     public void SaveLoad() throws IOException, ClassNotFoundException {
-        AppModel testmodel = new AppModel();
-        Participant[] part = new Participant[3];
-        Bin[] bins = new Bin[4];
-        for (int i = 0; i < part.length; i++) {
-            part[i] = new Participant("Participant " + i, "part" + i + "@test.com");
-        }
-        for (int i = 0; i < bins.length; i++) {
-            bins[i] = new Bin("Bin " + i);
-            bins[i].initialize(part.length);
-            bins[i].shareValue(BigInteger.valueOf(3 * i + 7));
-        }
-        testmodel.advanceState(AppState.STARTING);
-        testmodel.initializeStudy("Teststudy", part, bins);
+        AppModel testmodel = AppModelTest.getInitializedModel(3, 4);
         try {
             File fn = new File("testing.dat");
             testmodel.filename = fn;
