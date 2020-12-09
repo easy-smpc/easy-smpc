cbd577210d5635204b30f3542e7706f7bef7a7ae
Wirth, Felix Nikolaus <felix-nikolaus.wirth@charite.de>
2020-12-09 13:25:34 +0100

    Revert "Bugfix: Delete empty lines"
    This reverts commit 0beb5772dc205e2defcf2037cfeb84f9e3755707.

--- java/src/main/java/de/tu_darmstadt/cbs/app/Perspective1ACreate.java
+++ java/src/main/java/de/tu_darmstadt/cbs/app/Perspective1ACreate.java
@@ -102,24 +102,19 @@ public class Perspective1ACreate extends Perspective implements ChangeListener {
      * Removes empty lines in participants and bins
      */
     private void actionRemoveEmptyLines() {
+        //Remove participants if both fields empty
         for (Component entry : this.participants.getComponents()) {
-            if (this.participants.getComponentCount() > 1) {
-                // Remove participants if both fields empty
-                if (((EntryParticipant) entry).getLeftValue().trim().isEmpty() &&
-                    ((EntryParticipant) entry).getRightValue().trim().isEmpty()) {
-                    removeParticipant((EntryParticipant) entry);
-                }
+            if (((EntryParticipant) entry).getLeftValue().trim().isEmpty() &&
+                ((EntryParticipant) entry).getRightValue().trim().isEmpty()) {
+                removeParticipant((EntryParticipant) entry);
             }
         }
-
+        //Remove bin if left field empty and right field empty or zero
         for (Component entry : this.bins.getComponents()) {
-            if (this.bins.getComponentCount() > 1) {
-                // Remove bin if left field empty and right field empty or zero
-                if (((EntryBin) entry).getLeftValue().trim().isEmpty() &&
-                    ((((EntryBin) entry).getRightValue().trim().isEmpty()) ||
-                     ((EntryBin) entry).getRightValue().trim().equals(String.valueOf(0)))) {
-                    removeBin((EntryBin) entry);
-                }
+            if (((EntryBin) entry).getLeftValue().trim().isEmpty() &&
+                (((EntryBin) entry).getRightValue().trim().isEmpty()) ||
+                ((EntryBin) entry).getRightValue().trim().equals("0")) {
+                removeBin((EntryBin) entry);
             }
         }
     }
