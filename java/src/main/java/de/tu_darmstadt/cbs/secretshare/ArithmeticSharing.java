12424e60ab42d0349e89e188b0ef7a52d0ef04c6
Fabian Prasser <mail@fabian-prasser.de>
2021-01-24 15:07:30 +0100

    Sort members

--- java/src/main/java/de/tu_darmstadt/cbs/secretshare/ArithmeticSharing.java
+++ java/src/main/java/de/tu_darmstadt/cbs/secretshare/ArithmeticSharing.java
@@ -66,24 +66,6 @@ public class ArithmeticSharing {
     }
 
     /**
-     * Generate a signed blind
-     * @param bitlength Bitlength *including* sign bit
-     * @throws IllegalArgumentException Too small or negative bitlength
-     * @return BigInteger
-     */
-    private BigInteger getSignedBlind(int bitlength) throws IllegalArgumentException {
-      if (bitlength < 2)
-        throw new IllegalArgumentException("Bitlength must be larger than 2");
-      BigInteger value = new BigInteger(bitlength-1, randomGenerator);
-      byte[] randomByte = new byte[1];
-      randomGenerator.nextBytes(randomByte);
-      int signum = Byte.valueOf(randomByte[0]).intValue() & 0x01;
-      if (signum == 1)
-        value = value.negate();
-      return value;
-    }
-
-    /**
      * Share a secret
      * @param secret
      * @return
@@ -109,5 +91,23 @@ public class ArithmeticSharing {
      */
     public ArithmeticShare[] share(int secret) {
         return share(BigInteger.valueOf(secret));
+    }
+
+    /**
+     * Generate a signed blind
+     * @param bitlength Bitlength *including* sign bit
+     * @throws IllegalArgumentException Too small or negative bitlength
+     * @return BigInteger
+     */
+    private BigInteger getSignedBlind(int bitlength) throws IllegalArgumentException {
+      if (bitlength < 2)
+        throw new IllegalArgumentException("Bitlength must be larger than 2");
+      BigInteger value = new BigInteger(bitlength-1, randomGenerator);
+      byte[] randomByte = new byte[1];
+      randomGenerator.nextBytes(randomByte);
+      int signum = Byte.valueOf(randomByte[0]).intValue() & 0x01;
+      if (signum == 1)
+        value = value.negate();
+      return value;
     }
 }
