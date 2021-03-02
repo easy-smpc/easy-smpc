d40e0c936492128ea4fddb77f378e69745342adb
Wirth, Felix Nikolaus <felix-nikolaus.wirth@charite.de>
2021-03-02 14:45:11 +0100

    extend installers

--- createInstaller/createInstallerWindows.bat
+++ createInstaller/createInstallerWindows.bat
@@ -1,5 +1,5 @@
 REM ==================================
-REM	Create an EasySMPC installer for windows
+REM	Create an EasySMPC installer for Windows
 REM Usually only the version variable needs to be adapted
 REM Before executing the batch file the WIX toolset needs to be obtained and installed from https://wixtoolset.org/
 REM ==================================
@@ -11,8 +11,8 @@ set applicationName=EasySMPC
 set descriptionText="Tool to add sums in a secure manner"
 set vendor="Medical Informatics Group@Berlin Institute of Health and University of Technical University of Darmstadt"
 set copyright="Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0"
-REM probably add file associations
-set license-file="..\LICENSE"
+REM add file associations
+set licenseFile="..\LICENSE"
 REM create a desktop shortcut?
 REM add win-menu-group
 
@@ -21,11 +21,12 @@ jpackage --input %buildPath%^
 		 --main-class %mainClass%^
 		 --type exe^
 		 --dest %buildPath%^
-		 --win-per-user-install^
 		 --name %applicationName%^
 		 --app-version %version%^
 		 --description %descriptionText%^
+		 --vendor %VENDOR%^
 		 --copyright %copyright%^
-		 --license-file %license-file%^
+		 --license-file %licenseFile%^
 		 --win-dir-chooser^
-		 --win-menu
+		 --win-menu^
+		 --win-per-user-install^
