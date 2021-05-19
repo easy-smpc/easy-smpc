b4f14aea2eac72c673f7770c68fb78a5ef22483c
Wirth, Felix Nikolaus <felix-nikolaus.wirth@charite.de>
2021-05-19 10:38:40 +0200

    Make relevant Bus methods synchronized

--- src/main/java/org/bihealth/mi/easybus/Bus.java
+++ src/main/java/org/bihealth/mi/easybus/Bus.java
@@ -49,7 +49,7 @@ public abstract class Bus {
      * @param participant
      * @param messageListener
      */
-    public void receive(Scope scope, Participant participant, MessageListener messageListener) {
+    public synchronized void receive(Scope scope, Participant participant, MessageListener messageListener) {
         
         // Get or create scope
         Map<Participant,List<MessageListener>> subscriptionsForScope = subscriptions.get(scope);        
@@ -92,7 +92,7 @@ public abstract class Bus {
      * @param participant
      * @throws InterruptedException 
      */
-    protected boolean receiveInternal(Message message, Scope scope, Participant participant) throws InterruptedException {
+    protected synchronized boolean receiveInternal(Message message, Scope scope, Participant participant) throws InterruptedException {
         
         // Mark received
         boolean received = false;
