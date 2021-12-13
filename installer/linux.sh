44416e346d8ebdb89383706f3223461c7b288de3
Wirth, Felix Nikolaus <felix-nikolaus.wirth@charite.de>
2021-12-13 17:37:10 +0100

    Create version 1.0.1 to use secure version of log4j

--- installer/linux.sh
+++ installer/linux.sh
@@ -4,10 +4,10 @@
 #Usually only the version variable and the type (RPM or DEB) needs to be adapted
 #==================================
 VERSION="1.0"
-MINOR_VERSION="0" # if left out for deb packages a "1" is added
+MINOR_VERSION="1" # if left out for deb packages a "1" is added
 TYPE="deb"
 BUILD_PATH="../target"
-MAIN_JAR="easysmpc.jar"
+MAIN_JAR="easy-smpc-1.0.1.jar"
 MAIN_CLASS="org.bihealth.mi.easysmpc.App"
 APPLICATION_NAME="EasySMPC"
 DESCRIPTION_TEXT="No-Code Approach to Secure Multi-Party Computation"
