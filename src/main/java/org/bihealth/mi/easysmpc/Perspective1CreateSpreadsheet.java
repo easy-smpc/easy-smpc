f41dce0802a9f7cf7f956a44b068110f7e5ba6cd
Wirth, Felix Nikolaus <felix-nikolaus.wirth@charite.de>
2022-01-16 00:32:00 +0100

    Make buttons usable

--- src/main/java/org/bihealth/mi/easysmpc/Perspective1CreateSpreadsheet.java
+++ src/main/java/org/bihealth/mi/easysmpc/Perspective1CreateSpreadsheet.java
@@ -17,8 +17,6 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.IOException;
@@ -122,17 +120,6 @@ public class Perspective1CreateSpreadsheet extends Perspective implements ListSe
             @Override
             public void changedUpdate(DocumentEvent e) {
                 ((SpreadsheetCellEditor) table.getCellEditor(table.getSelectedRow(), table.getSelectedColumn())).setTextField(formulaField.getText());
-            }
-        });
-        formulaField.addFocusListener(new FocusListener() {
-            @Override
-            public void focusLost(FocusEvent e) {
-                table.editingCanceled(null);
-            }
-            
-            @Override
-            public void focusGained(FocusEvent e) {
-                // Empty
             }
         });
         
