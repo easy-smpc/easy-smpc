661ebc547274ecdd9bce9a0f74846623066e813a
wirthfn <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>
2022-08-01 19:10:20 +0200

    Update to version 1.0.8

--- src/main/java/org/bihealth/mi/easysmpc/App.java
+++ src/main/java/org/bihealth/mi/easysmpc/App.java
@@ -69,7 +69,7 @@ import de.tu_darmstadt.cbs.emailsmpc.Study.StudyState;
  */
 public class App extends JFrame {
     
-    public static final String VERSION = "1.0.7";
+    public static final String VERSION = "1.0.8";
 
     /** SVUID */
     private static final long serialVersionUID = 8047583915796168387L;
